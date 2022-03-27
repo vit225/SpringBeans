ab70d7d2a251547d83e9cc653bf129119161323f
vit225 <vitaly1090@mail.ru>
2022-03-27 16:04:11 +1000

    commit

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Cat extends Animal {
+
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "I'm a Cat";
     }
 }
